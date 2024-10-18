7b1d082df7718b591a942e629c6d36651ab1ba62
Tanh Nguyen <66580316+nvtanhh@users.noreply.github.com>
2024-10-18 16:33:25 +0700

    Update 0238-product_of_array_except_self.ts

--- src/0238-product_of_array_except_self.ts
+++ src/0238-product_of_array_except_self.ts
@@ -1,36 +1,82 @@
 function productExceptSelf(nums: number[]): number[] {
 
+    // ##### Approach 1: using division operation, but its not allow to use
+    // division operation for this problem
+
+    // const n = nums.length;
+    // let totalProduct = 1;
+    // const zeroNumIndexes = [];
+
+    // for (let i = 0; i < n; i++) {
+    //     const num = nums[i];
+    //     if (num != 0) {
+    //         totalProduct *= num;
+    //     } else {
+    //         zeroNumIndexes.push(i)
+    //     }
+    // }
+
+    // const answer = new Array(n).fill(0)
+
+    // if (zeroNumIndexes.length) {
+    //     if (zeroNumIndexes.length == 1) {
+    //         answer[zeroNumIndexes[0]] = totalProduct
+    //     }
+    //     return answer
+    // }
+
+    // for (let i = 0; i < n; i++) {
+    //     answer[i] = totalProduct / nums[i];
+    // }
+
+    // return answer;
+
+
+    // ##### Approach 2: using prefix-postfix array to store values
+    // const n = nums.length;
+
+    // const prefixArray = [];
+    // let prefix = 1;
+    // for (let i = 0; i < n; i++) {
+    //     prefix *= nums[i];
+    //     prefixArray[i] = prefix;
+    // }
+
+    // const postfixArray = [];
+    // let postfix = 1;
+    // for (let i = n - 1; i >= 0; i--) {
+    //     postfix *= nums[i]
+    //     postfixArray[i] = postfix;
+    // }
+
+    // const answer = [];
+    // for (let i = 0; i < n; i++) {
+    //     prefix = i != 0 ? prefixArray[i - 1] : 1;
+    //     postfix = i != n - 1 ? postfixArray[i + 1] : 1;
+
+    //     answer[i] = prefix * postfix;
+    // }
+
+    // return answer;
+
+    //  ##### Approach 3: using answer array to store value 
     const n = nums.length;
-    let totalProduct = 1;
-    const zeroNumIndexes = [];
+    const answer = [].fill(1);
 
+    let product = 1;
     for (let i = 0; i < n; i++) {
-        const num = nums[i];
-
-        if (num != 0) {
-            totalProduct *= num;
-        } else {
-            zeroNumIndexes.push(i)
-        }
+        answer[i] = product;
+        product *= nums[i];
     }
 
-    const answer = new Array(n).fill(0)
-
-    if (zeroNumIndexes.length) {
-        if (zeroNumIndexes.length == 1) {
-            answer[zeroNumIndexes[0]] = totalProduct
-        }
-        return answer
-    }
-
-
-
-    for (let i = 0; i < n; i++) {
-        answer[i] = totalProduct / nums[i];
+    product = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= product;
+        product *= nums[i];
     }
 
     return answer;
-};
+}
 
 
 export function testProductExceptSelf() {
