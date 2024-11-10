f204ccb836815c3d2a5ce8410c4ad2659557e9c6
Tanh Nguyen <nvtanh.dev@gmail.com>
2024-11-10 23:41:18 +0700

    Update 0832-flipping_an_image.ts

--- src/twoPointers/0832-flipping_an_image.ts
+++ src/twoPointers/0832-flipping_an_image.ts
@@ -1,3 +1,16 @@
 function flipAndInvertImage(image: number[][]): number[][] {
-    return image.map(row => row.reverse().map(pixel => pixel ^ 1));
+    // return image.map(row => row.reverse().map(pixel => pixel ^ 1));
+
+
+    for (let row of image) {
+        let l = 0;
+        let r = row.length - 1;
+
+        while (l < r) {
+            [row[l], row[r]] = [row[r] ^ 1, row[l] ^ 1];
+            l++;
+            r--;
+        }
+    }
+    return image;
 };
