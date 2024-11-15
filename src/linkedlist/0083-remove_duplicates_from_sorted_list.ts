28ea89e5f24f3627700904c94c725764e231f600
Tanh Nguyen <nvtanh.dev@gmail.com>
2024-11-15 23:57:38 +0700

    Update 0083-remove_duplicates_from_sorted_list.ts

--- src/linkedlist/0083-remove_duplicates_from_sorted_list.ts
+++ src/linkedlist/0083-remove_duplicates_from_sorted_list.ts
@@ -1,25 +1,17 @@
 import { ListNode } from "../models/listnode";
 
 function deleteDuplicates(head: ListNode | null): ListNode | null {
-    let prev = null;
     let curr = head;
 
     while (curr != null) {
-        if (prev != null && prev.val == curr.val ) {
-            curr = curr.next;
-            prev.next = curr;
-            curr = curr.next;
-            continue;
+        let next = curr.next;
+        while (next != null && next.val === curr.val) {
+            next = next.next;
         }
 
-        if(curr == null){
-            break;
-        }
-
-        prev = curr;
+        curr.next = next;
         curr = curr.next;
     }
 
     return head;
-
-};
+}
