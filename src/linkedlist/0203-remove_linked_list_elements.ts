9024e3375c3d0a0fd76dac12bb60f9e0105fdea8
Tanh Nguyen <nvtanh.dev@gmail.com>
2024-11-20 00:08:55 +0700

    Create 0203-remove_linked_list_elements.ts

---
+++ src/linkedlist/0203-remove_linked_list_elements.ts
@@ -0,0 +1,19 @@
+import { ListNode } from "../models/listnode";
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let curr = dummy;
+
+    while (curr != null) {
+        let next = curr.next;
+        while (next != null && next.val === val) {
+            next = next.next;
+        }
+
+        curr.next = next;
+        curr = curr.next;
+    }
+
+    return dummy.next;
+};
